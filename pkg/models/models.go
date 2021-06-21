265c8fc0f70a1b07276329bff01f832718dab04d
Andres Morey <andresmarcel@gmail.com>
2021-06-21 14:41:25 +0300

    improved cf-stack.yaml

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -1,9 +1,13 @@
 package models
 
+// TODO:
+// Spot notice schema: https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/spot-interruptions.html
+// ASG event schema: https://docs.aws.amazon.com/autoscaling/ec2/userguide/cloud-watch-events.html#terminate-lifecycle-action
+
 type EventBridgeEvent struct {
         Detail EventBridgeEventDetail `json:"detail"`
 }
 
 type EventBridgeEventDetail struct {
-	InstanceId string `json:"instance-id"`
+	InstanceId string `json:"EC2InstanceId"`
 }
